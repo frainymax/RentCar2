7ab7f3143cb842b856c1b8592ca9c67fa719301d
User <User@DESKTOP-2MA536R>
2026-03-26 14:12:10 -0400

    prueba

---
+++ src/main/java/com/mycompany/rentcar/RentCar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rentcar;
+
+/**
+ *
+ * @author User
+ */
+public class RentCar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
